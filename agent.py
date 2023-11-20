dda3dd494f65b6d5b0e1515d94a537dcdda144ac
Mohamed Ghoneim <138689320+moghon92@users.noreply.github.com>
2023-11-20 22:49:07 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -263,8 +263,8 @@ class Assistant():
 def chat_with_Anna(query, hotelDoc, chat_history, system_msg):
 #if __name__=="__main__":
     
-    os.environ["OPENAI_API_KEY"] = "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"
-    os.environ["LANGCHAIN_TRACING"] = "false"
+    # os.environ["OPENAI_API_KEY"] = "sk----"
+    # os.environ["LANGCHAIN_TRACING"] = "false"
     
     retriever = Document(raw_text=hotelDoc, index_name='hotel_momo').as_retriever(top_k=3)
     tools = AssistantTools().tools
